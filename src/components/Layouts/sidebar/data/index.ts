028db83adb6040a4d97b5a2b4fe6177301d45eee
Camalu Atacado <camaluatacado@gmail.com>
2025-02-10 10:38:39 -0300

    update

--- src/components/Layouts/sidebar/data/index.ts
+++ src/components/Layouts/sidebar/data/index.ts
@@ -2,72 +2,74 @@ import * as Icons from "../icons";
 
 export const NAV_DATA = [
   {
-    label: "MAIN MENU",
+    label: "MENU PRINCIPAL",
     items: [
       {
         title: "Dashboard",
         icon: Icons.HomeIcon,
-        items: [
-          {
-            title: "eCommerce",
-            url: "/",
-          },
-        ],
-      },
-      {
-        title: "Calendar",
-        url: "/calendar",
-        icon: Icons.Calendar,
+        url: "/",
         items: [],
       },
       {
-        title: "Profile",
-        url: "/profile",
-        icon: Icons.User,
+        title: "Adquirentes",
+        icon: Icons.ChevronUp,
+        url: "/adquirentes",
         items: [],
       },
-      {
-        title: "Forms",
-        icon: Icons.Alphabet,
-        items: [
-          {
-            title: "Form Elements",
-            url: "/forms/form-elements",
-          },
-          {
-            title: "Form Layout",
-            url: "/forms/form-layout",
-          },
-        ],
-      },
-      {
-        title: "Tables",
-        url: "/tables",
-        icon: Icons.Table,
-        items: [
-          {
-            title: "Tables",
-            url: "/tables",
-          },
-        ],
-      },
-      {
-        title: "Pages",
-        icon: Icons.Alphabet,
-        items: [
-          {
-            title: "Settings",
-            url: "/pages/settings",
-          },
-        ],
-      },
+      // {
+      //   title: "Calendar",
+      //   url: "/calendar",
+      //   icon: Icons.Calendar,
+      //   items: [],
+      // },
+      // {
+      //   title: "Profile",
+      //   url: "/profile",
+      //   icon: Icons.User,
+      //   items: [],
+      // },
+      // {
+      //   title: "Forms",
+      //   icon: Icons.Alphabet,
+      //   items: [
+      //     {
+      //       title: "Form Elements",
+      //       url: "/forms/form-elements",
+      //     },
+      //     {
+      //       title: "Form Layout",
+      //       url: "/forms/form-layout",
+      //     },
+      //   ],
+      // },
+      // {
+      //   title: "Tables",
+      //   url: "/tables",
+      //   icon: Icons.Table,
+      //   items: [
+      //     {
+      //       title: "Tables",
+      //       url: "/tables",
+      //     },
+      //   ],
+      // },
+      // {
+      //   title: "Pages",
+      //   icon: Icons.Alphabet,
+      //   items: [
+      //     {
+      //       title: "Settings",
+      //       url: "/pages/settings",
+      //     },
+      //   ],
+      // },
     ],
   },
   {
-    label: "OTHERS",
+    label: "CONFIGURAÇÕES",
     items: [
       {
-        title: "Charts",
+        title: "Conta",
         icon: Icons.PieChart,
         items: [
           {
@@ -76,30 +78,30 @@ export const NAV_DATA = [
           },
         ],
       },
-      {
-        title: "UI Elements",
-        icon: Icons.FourCircle,
-        items: [
-          {
-            title: "Alerts",
-            url: "/ui-elements/alerts",
-          },
-          {
-            title: "Buttons",
-            url: "/ui-elements/buttons",
-          },
-        ],
-      },
-      {
-        title: "Authentication",
-        icon: Icons.Authentication,
-        items: [
-          {
-            title: "Sign In",
-            url: "/auth/sign-in",
-          },
-        ],
-      },
+      // {
+      //   title: "UI Elements",
+      //   icon: Icons.FourCircle,
+      //   items: [
+      //     {
+      //       title: "Alerts",
+      //       url: "/ui-elements/alerts",
+      //     },
+      //     {
+      //       title: "Buttons",
+      //       url: "/ui-elements/buttons",
+      //     },
+      //   ],
+      // },
+      // {
+      //   title: "Authentication",
+      //   icon: Icons.Authentication,
+      //   items: [
+      //     {
+      //       title: "Sign In",
+      //       url: "/auth/sign-in",
+      //     },
+      //   ],
+      // },
     ],
   },
 ];
